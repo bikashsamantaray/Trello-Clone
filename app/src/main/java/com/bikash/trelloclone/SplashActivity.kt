10ad2b617473743f55a9aefe2b65e6f6d0023ff9
bikashsamantaray <samantaraybikash2015@gmail.com>
2022-09-06 11:27:24 +0530

    app

--- app/src/main/java/com/bikash/trelloclone/SplashActivity.kt
+++ app/src/main/java/com/bikash/trelloclone/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.bikash.trelloclone
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 import android.widget.TextView
 
@@ -23,6 +25,10 @@ class SplashActivity : AppCompatActivity() {
         val typeFace: Typeface = Typeface.createFromAsset(assets, "SpectralSC-ExtraBoldItalic.ttf")
         tvAppName?.typeface = typeFace
 
+        Handler().postDelayed({
+            startActivity(Intent(this,IntroActivity::class.java))
+            finish()
+        },2500)
 
 
     }
