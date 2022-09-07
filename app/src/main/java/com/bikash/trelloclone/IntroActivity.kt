ad0e7bfcca6cd9dd16108ca153eabe0e0d4d5bc5
bikashsamantaray <samantaraybikash2015@gmail.com>
2022-09-08 01:04:11 +0530

    https://github.com/bikashsamantaray/Trello-Clone

--- app/src/main/java/com/bikash/trelloclone/IntroActivity.kt
+++ app/src/main/java/com/bikash/trelloclone/IntroActivity.kt
@@ -1,11 +1,23 @@
 package com.bikash.trelloclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
 
 class IntroActivity : AppCompatActivity() {
+    private var btnSignUp: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+        btnSignUp = findViewById(R.id.btn_sign_up_intro)
+
+
+        btnSignUp?.setOnClickListener{
+            startActivity(Intent(this@IntroActivity,SignUpActivity::class.java))
+        }
+
+
     }
 }
--- app/src/main/java/com/bikash/trelloclone/SignUpActivity.kt
+++ app/src/main/java/com/bikash/trelloclone/SignUpActivity.kt
@@ -2,10 +2,30 @@ package com.bikash.trelloclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.bikash.trelloclone.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private var binding: ActivitySignUpBinding? = null
+    private var toolbarSignUpActivity: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding?.toolbarSignUpActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null){
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarSignUpActivity?.setNavigationOnClickListener{
+            onBackPressed()
+        }
+
     }
 }
