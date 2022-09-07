ce86ebdccd406ee5512f92495f8c60f3d11a9c76
bikashsamantaray <samantaraybikash2015@gmail.com>
2022-09-08 02:19:20 +0530

    hell

--- app/src/main/java/com/bikash/trelloclone/IntroActivity.kt
+++ app/src/main/java/com/bikash/trelloclone/IntroActivity.kt
@@ -8,14 +8,24 @@ import android.widget.TextView
 
 class IntroActivity : AppCompatActivity() {
     private var btnSignUp: TextView? = null
+    private var btnSignIn: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
         btnSignUp = findViewById(R.id.btn_sign_up_intro)
+        btnSignIn = findViewById(R.id.btn_sign_in_intro)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
 
         btnSignUp?.setOnClickListener{
             startActivity(Intent(this@IntroActivity,SignUpActivity::class.java))
+        }
+        btnSignIn?.setOnClickListener{
+            startActivity(Intent(this@IntroActivity,SignInActivity::class.java))
         }
 
 
--- app/src/main/java/com/bikash/trelloclone/SignUpActivity.kt
+++ app/src/main/java/com/bikash/trelloclone/SignUpActivity.kt
@@ -2,16 +2,20 @@ package com.bikash.trelloclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.TextView
 import com.bikash.trelloclone.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
     private var binding: ActivitySignUpBinding? = null
-    private var toolbarSignUpActivity: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setupActionBar()
     }
 
