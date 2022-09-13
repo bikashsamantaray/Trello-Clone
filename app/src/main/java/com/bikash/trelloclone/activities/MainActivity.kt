a1fa7c229a4542d8ce4b29dbe8f328b495403649
bikashsamantaray <samantaraybikash2015@gmail.com>
2022-09-14 01:38:01 +0530

    k

--- app/src/main/java/com/bikash/trelloclone/activities/MainActivity.kt
+++ app/src/main/java/com/bikash/trelloclone/activities/MainActivity.kt
@@ -2,13 +2,35 @@ package com.bikash.trelloclone.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 import com.bikash.trelloclone.R
+import com.bikash.trelloclone.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity() {
+    private var binding:ActivityMainBinding? = null
+    var appbarMain:Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        appbarMain = findViewById(R.id.toolbar_main_activity)
+        setContentView(binding?.root)
 
-        showErrorSnackBar("signed in successfully")
+
+        //showErrorSnackBar("signed in successfully")
     }
+
+    private fun setupActionBar(){
+        setSupportActionBar(appbarMain)
+        appbarMain?.setNavigationIcon(R.drawable.)
+
+    }
+
+    private fun setSupportActionBar(appbarMain: Toolbar?) {
+
+    }
+
+
+}
+
 }
