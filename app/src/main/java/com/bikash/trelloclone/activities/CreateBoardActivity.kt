198936092c9069e838f227d5ec459de0ae081d93
BIKASH <samantaraybikash2015@gmail.com>
2022-11-11 23:34:35 +0530

    hell

--- app/src/main/java/com/bikash/trelloclone/activities/CreateBoardActivity.kt
+++ app/src/main/java/com/bikash/trelloclone/activities/CreateBoardActivity.kt
@@ -6,22 +6,28 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.bikash.trelloclone.R
 import com.bikash.trelloclone.databinding.ActivityCreateBoardBinding
 import com.bikash.trelloclone.databinding.ActivityMyProfileBinding
+import com.bikash.trelloclone.firebase.FireStoreClass
+import com.bikash.trelloclone.models.Board
 import com.bikash.trelloclone.utils.Constants
 import com.bikash.trelloclone.utils.Constants.READ_STORAGE_PERMISSION_CODE
 import com.bikash.trelloclone.utils.Constants.showImageChooser
 import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import de.hdodenhof.circleimageview.CircleImageView
 
 class CreateBoardActivity : BaseActivity() {
     private var mSelectedImageUri: Uri? = null
     private lateinit var mUserName: String
-    //private var mBoardImageUrl
+    private var mBoardImageUrl: String = ""
+
     private var binding: ActivityCreateBoardBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,46 +53,92 @@ class CreateBoardActivity : BaseActivity() {
             }
         }
 
+        binding?.btnCreate?.setOnClickListener {
+            if (mSelectedImageUri != null){
+                uploadBoardImage()
+            }else{
+                showProgressDialog(resources.getString(R.string.please_wait))
+                createBoard()
+            }
+        }
+
+    }
+
+    private fun setupActionBar() {
+            setSupportActionBar(binding?.toolbarCreateBoardActivity)
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_new_24)
+            supportActionBar?.title = resources.getString(R.string.create_board_title)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+            binding?.toolbarCreateBoardActivity?.setNavigationOnClickListener{
+                onBackPressed()
+            }
+
     }
 
     private fun createBoard(){
+        val assignedUserArrayList: ArrayList<String> = ArrayList()
+        assignedUserArrayList.add(getCurrentUserId())
 
+        val board = Board(
+            binding?.etBoardName?.text.toString(),
+            mBoardImageUrl,
+            mUserName,
+            assignedUserArrayList
+
+        )
+
+        FireStoreClass().createBoard(this,board)
+
+    }
+
+    private fun uploadBoardImage() {
+        showProgressDialog(resources.getString(R.string.please_wait))
+        val sRef: StorageReference = FirebaseStorage.getInstance()
+            .reference.child(
+                "BOARD_IMAGE" + System.currentTimeMillis() + "." + Constants.getFileExtensions(
+                    this,
+                    this.mSelectedImageUri
+                )
+            )
+
+
+        sRef.putFile(mSelectedImageUri!!).addOnSuccessListener { taskSnapshot ->
+            Log.i("Board Image Url", taskSnapshot.metadata!!.reference!!.downloadUrl.toString())
+            taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener { uri ->
+                Log.i("Downloadable image URL", uri.toString())
+                mBoardImageUrl = uri.toString()
+                createBoard()
+
+            }
+
+
+        }.addOnFailureListener { exception ->
+            Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+
+            hideProgressDialog()
+        }
     }
 
     fun boardCreatedSuccessFully(){
         hideProgressDialog()
         finish()
     }
-
-
-
-    private fun setupActionBar(){
-        setSupportActionBar(binding?.toolbarCreateBoardActivity)
-        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_new_24)
-        supportActionBar?.title = resources.getString(R.string.create_board_title)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        binding?.toolbarCreateBoardActivity?.setNavigationOnClickListener{
-            onBackPressed()
-        }
-
-    }
-
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == READ_STORAGE_PERMISSION_CODE){
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                showImageChooser(this)
-            }
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+        ) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            if (requestCode == READ_STORAGE_PERMISSION_CODE){
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    showImageChooser(this)
+                }
 
-        }else{
-            Toast.makeText(this,"oops you denied the permissions",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,"oops you denied the permissions",Toast.LENGTH_SHORT).show()
+            }
         }
-    }
 
 
     @Deprecated("Deprecated in Java")
